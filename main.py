e3f4cd7093048d7ddb268fd5d6727309e9a5c7d5
ALBY <68139513+ALBINPRAVEEN@users.noreply.github.com>
2021-03-16 10:05:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
-#!/bin/env python3
-
-import os, sys
-sys.path.append(os.getcwd()+"/.lib/")
-import argparse
 from api import *
 
 ap = argparse.ArgumentParser()
-ap.add_argument("-u", "--user", required=True, help="username of account to scan")
-ap.add_argument("-p", "--post", action="store_true", help="image info of user uploads")
+ap.add_argument("-u", "--username", required=True, help="username of account to scan")
+ap.add_argument("-p", "--postscrap", action='store_true', help="scrape all uploaded images info ")
+ap.add_argument("-s", "--savedata", action='store_true', help="save data to file")
+ap.add_argument("-t", "--tagscrap", action="store_true", help="list often used tags")
 args = vars(ap.parse_args())
-	
+
 os.system("clear")
+ig = main(user=args["username"])
+if args['tagscrap']:
+	ig.print_data()
+else:
+	ig.print_data_()
 
-if args['user']:
-	user_info(usrname=args["user"])
+if args['postscrap']:
+	ig.scrap_uploads()
 
-if args['post']:
-	post_info()
+if args['savedata']:
+	ig.save_data()
