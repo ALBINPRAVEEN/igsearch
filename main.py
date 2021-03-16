dae797b383474204bfb6267d64462ff11387fdd0
ALBY <68139513+ALBINPRAVEEN@users.noreply.github.com>
2021-03-16 10:09:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+#!/bin/env python3
+
+import os, sys
+sys.path.append(os.getcwd()+"/.lib/")
+import argparse
 from api import *
 
 ap = argparse.ArgumentParser()
@@ -6,7 +11,7 @@ ap.add_argument("-p", "--postscrap", action='store_true', help="scrape all uploa
 ap.add_argument("-s", "--savedata", action='store_true', help="save data to file")
 ap.add_argument("-t", "--tagscrap", action="store_true", help="list often used tags")
 args = vars(ap.parse_args())
-
+	
 os.system("clear")
 ig = main(user=args["username"])
 if args['tagscrap']:
